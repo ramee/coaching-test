5b1dc0917eab764c33c78b8817dba7bc7570aa72
Ramon Nagy <nagy.ramon@gmail.com>
2022-05-29 14:42:39 +0200

    fix: change to use prefix on route-group

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group([], static function () {
+Route::group(['prefix' => 'user'], static function () {
     // todo coach's availability
-})->prefix('user');
+});
